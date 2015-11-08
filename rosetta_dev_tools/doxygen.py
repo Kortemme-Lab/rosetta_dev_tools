23618892de49306e2bdadff3d6309abb0543fcdf
Kale Kundert <kale@thekunderts.net>
2015-11-08 09:17:48 -0800

    Modernize the doxygen script.

--- rosetta_dev_tools/doxygen.py
+++ rosetta_dev_tools/doxygen.py
@@ -1,35 +1,59 @@
 #!/usr/bin/env python3
 
-import os
-import shutil
-import helpers
-import argparse
+"""\
+Generate documentation for a small number of files you're actively developing.
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--fast', '-f', action='store_true')
-parser.add_argument('--quiet', '-q', action='store_true')
-arguments = parser.parse_args()
+Usage:
+    rdt_doxygen [<directory>] [options]
 
-root = '/tmp/rosetta_doxygen'
-html = os.path.join(root, 'html', 'annotated.html')
+Arguments:
+    <directory>         [default: .]
+        The directory to generate documentation for.
 
-if os.path.exists(root):
-    shutil.rmtree(root)
+Options:
+    -r, --recursive
+        Recurse into subdirectories of the specified directory.  This is off by 
+        default, because it can be noticeably slower.
 
-os.mkdir(root)
+    -q, --quiet
+        Don't launch a new Firefox window after generating the documentation.  
+        Usually you wouldn't specify this option the first time you run this 
+        command, because otherwise you'd have to open a web browser window 
+        yourself.  After that, though, you would specify this option and reuse 
+        the window you already have.
+"""
 
-doxygen_config = [
-        'OUTPUT_DIRECTORY = ' + root,
-        'EXTRACT_ALL = YES',
-        'EXTRACT_PRIVATE = YES',
-        'RECURSIVE = ' + 'YES' if not arguments.fast else 'NO',
-        'SOURCE_BROWSER = YES',
-        'INLINE_SOURCES = YES',
-]
-doxygen_command =  'echo "{}" | doxygen -'.format('\n'.join(doxygen_config))
-firefox_command = 'firefox -new-window {}'.format(html)
+import os, shutil, docopt
+from . import helpers
 
-helpers.shell_command('.', doxygen_command)
-if not arguments.quiet:
-    helpers.shell_command('.', firefox_command)
+def main():
+    args = docopt.docopt(__doc__)
+    target_dir = args['<directory>'] or '.'
+
+    # Make a temporary directory for the doxygen build.
+
+    root = '/tmp/rosetta_doxygen'
+    html = os.path.join(root, 'html', 'annotated.html')
+    if os.path.exists(root):
+        shutil.rmtree(root)
+    os.mkdir(root)
+
+    # Run doxygen on the specified directory, then show the resulting 
+    # documentation in a Firefox window (unless the user specified '-q').
+
+    doxygen_config = [
+            'OUTPUT_DIRECTORY = ' + root,
+            'EXTRACT_ALL = YES',
+            'EXTRACT_PRIVATE = YES',
+            'RECURSIVE = ' + 'YES' if not args['--recursive'] else 'NO',
+            'SOURCE_BROWSER = YES',
+            'INLINE_SOURCES = YES',
+            'GENERATE_HTML = YES',
+    ]
+    doxygen_command =  'echo "{}" | doxygen -'.format('\n'.join(doxygen_config))
+    firefox_command = 'firefox -new-window {}'.format(html)
+
+    helpers.shell_command(target_dir, doxygen_command)
+    if not args['--quiet']:
+        helpers.shell_command(target_dir, firefox_command)
 
